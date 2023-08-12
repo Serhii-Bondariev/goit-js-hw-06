b19166adbb9d336ffe501d235cb4007ec8f33ad4
Serhii Bondariev <xx.code4u@gmail.com>
2023-08-12 16:06:03 +0300

    ddd

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Будь ласка, заповніть усі поля");
+    return;
+  }
+
+  const loginData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(loginData);
+  loginForm.reset();
+});
